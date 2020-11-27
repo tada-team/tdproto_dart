5428f79c9fe1be2edd932564673a4fddac2827b1
Maxim Oransky <maxim.oransky@gmail.com>
2020-11-27 05:00:31 +0300

    upload (broken)

--- lib/tdproto_dart.dart
+++ lib/tdproto_dart.dart
@@ -3,3 +3,4 @@ library tdproto_dart;
 export './src/markup_entity/markup_entity.dart';
 export './src/pdf_version/pdf_version.dart';
 export './src/upload_preview/upload_preview.dart';
+export './src/upload_preview/upload.dart';
