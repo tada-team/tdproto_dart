379147a517887906a0fe499fdce0fe095d747a7e
Maxim Oransky <maxim.oransky@gmail.com>
2020-12-05 03:43:54 +0300

    country

--- lib/tdproto_dart.dart
+++ lib/tdproto_dart.dart
@@ -1,12 +1,14 @@
 library tdproto_dart;
 
+export './src/date_time_converter.dart';
+
+// generated:
 export './src/markup_type/markup_type.dart';
 export './src/markup_entity/markup_entity.dart';
 export './src/pdf_version/pdf_version.dart';
 export './src/upload_preview/upload_preview.dart';
 export './src/upload/upload.dart';
-export './src/date_time_converter.dart';
-
 export './src/chat_type/chat_type.dart';
 export './src/group_status/group_status.dart';
 export './src/team_status/team_status.dart';
+export './src/country/country.dart';
